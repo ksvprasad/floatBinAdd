cfb970227eb8a6493c127f25e55a707b66d4016c
Sai Khandavilli <Sai.khandavilli@ggktech.local>
2018-05-10 15:37:25 +0530

    Final

--- floatBinAdd/floatBinAdd/Program.cs
+++ floatBinAdd/floatBinAdd/Program.cs
@@ -12,42 +12,42 @@ namespace floatBinAdd
                     double num2 = double.Parse(Console.ReadLine());
                     int precision=8;            //Precision is set to compute a fixed number of digits after decimal point.                          
                     Program p = new Program(); //A constructor to actually be able to access the methods within "Program" class
-                    string str1 = p.decimalToBinary(num1, precision);   //A method to convert the user's decimal input to binary representation.
-                    string str2 = p.decimalToBinary(num2, precision);   //The output obtained is then stored in two strings(one for each input).   
+                    string binaryString1 = p.decimalToBinary(num1, precision);   //A method to convert the user's decimal input to binary representation.
+                    string binaryString2 = p.decimalToBinary(num2, precision);   //The output obtained is then stored in two strings(one for each input).   
                     Console.WriteLine("The Binary Sum of {0} and {1} is:",num1, num2);
                     String Sum; //A string to store the binary sum of the binary numbers obtained above.
                     if ((num1 > 0) && (num2 > 0))       //A case for two positive numbers.
                         {
-                            Sum = p.addition(str1, str2);     //An addition method to actually compute the sum.  
+                            Sum = p.addition(binaryString1, binaryString2);     //An addition method to actually compute the sum.  
                         }
                     else if ((num1 > 0) && (num2 < 0))  //A case for one positive and one negative number.
                         {
                             if (Math.Abs(num2) > num1)      //To obtain the absolute value of the number (for easy computation).
                                 {
-                                    Sum = p.subtraction(str2, str1);    //A subtraction method to compute the difference.
+                                    Sum = p.subtraction(binaryString2, binaryString1);    //A subtraction method to compute the difference.
                                 } 
                             else
                                 {
-                                    Sum = p.subtraction(str1, str2);    //Binary strings are sent in an order. (bigger number, smaller number).
+                                    Sum = p.subtraction(binaryString1, binaryString2);    //Binary strings are sent in an order. (bigger number, smaller number).
                                 }
                         }
                     else if ((num1 < 0) && (num2 > 0))  //Another case for one positive and one negative number.
                         {
                             if (Math.Abs(num1) > num2)
                                 {
-                                    Sum = p.subtraction(str1, str2);
+                                    Sum = p.subtraction(binaryString1, binaryString2);
                                 }
                             else
                                 {
-                                    Sum = p.subtraction(str2, str1);
+                                    Sum = p.subtraction(binaryString2, binaryString1);
                                 }
                         }
                     else
                         {
-                            Sum = p.addition(str1, str2);
+                            Sum = p.addition(binaryString1, binaryString2);
                         }
                     int size = Sum.Length -1;
-                    String intFracBin = "";
+                    String intFracBin = "";                         //A variable to store both the integral and fractional part of a binary number.
                     for (int i = size; i >= 0; i--)
                         {                                           //Appending Floating Point.
                             intFracBin = Sum[i] + intFracBin;       //Binary number with both integral and fractional parts.
@@ -76,7 +76,7 @@ namespace floatBinAdd
                     Console.ReadKey();
                     return 0;
                 }
-            string decimalToBinary(double num, int k_prec)
+            string decimalToBinary(double num, int k_precision)
                 {
                     string Integ = "";                    //Separate strings for integral and fractional parts of the number.
                     string Frac = "";
@@ -89,7 +89,7 @@ namespace floatBinAdd
                                 Integ=rem+Integ;                        //and adding them to the integral string.
                                 Integral = Integral / 2;
                             }
-                        while(k_prec>0)                     //A set precedence is used to denote the number of digits after the decimal point.
+                        while(k_precision>0)                     //A set precedence is used to denote the number of digits after the decimal point.
                             {
                                 fractional = fractional * 2;        //Mathematical method to compute the binary version of a fractional part.
                                 int bit = (int)fractional;          //Obtains the carried bit.
@@ -100,7 +100,7 @@ namespace floatBinAdd
                                         }
                                     else
                                         Frac = Frac + 0;
-                                k_prec--;
+                                k_precision--;
                             }
                     binary = Integ + Frac;                     //finally collaborates integral and fractional parts of the binary number obtained.
                     return binary;
@@ -140,30 +140,30 @@ namespace floatBinAdd
                     result = result.Remove(0, 1);//First Character is the Overflow Digit occured by 2's complement and hence should be removed...
                     return result;      //The subtracted result is returned.
                 }
-            int makeEqualLength(ref string str1, ref string str2)       //An important method to bring both the strings to equal length
+            int makeEqualLength(ref string binary1, ref string binary2)       //An important method to bring both the strings to equal length
                 {                                                          //by appending 0's at the beginning of the string which is smaller in length.
                     int i;                                              
-                    int len1 = str1.Length;                                //Initially the lengths of both the strings are obtained and stored in len1 and len2.
-                    int len2 = str2.Length;
+                    int len1 = binary1.Length;                                //Initially the lengths of both the strings are obtained and stored in len1 and len2.
+                    int len2 = binary2.Length;
                         if(len1 < len2)                         
                             {
                                 for(i = 0; i < len2 - len1; i++)                //len2-len1 obtains the difference in lengths if string1's length is less than that of string 2.
-                                    str1 = '0' + str1;                          //Adds 0 at the beginning if string 1 is smaller in length.   
+                                    binary1 = '0' + binary1;                          //Adds 0 at the beginning if string 1 is smaller in length.   
                                 return len2;                                //Returns the integer containing the length of the bigger string(in this case len2).
                             }
                         else if(len1 > len2)
                             {
                                 for(i = 0; i < len1 - len2; i++)                //len1-len2 obtains the difference in lengths if string2's length is less than that of string 1.
-                                    str2 = '0' + str2;                          //Adds 0 at the beginning if string1 is smaller in length.
+                                    binary2 = '0' + binary2;                          //Adds 0 at the beginning if string1 is smaller in length.
                                 return len1;                                //Returns the integer containing the length of the bigger string(in this case len1).
                             }
                         else
                             return len1;                                        //Returns the length of string1 if both strings are of the same size.
                 }
-            double binaryToDecimal(BigInteger Binary,int k_prec)        //A method to finally convert back the binary sum to a decimal format.
+            double binaryToDecimal(BigInteger Binary,int k_precision)        //A method to finally convert back the binary sum to a decimal format.
                 { 
                     double Decimal = 0;                                 //A variable to store the result.                        
-                    double kprecision = -k_prec;                        //As fractional bits hold powers in negative exponents.
+                    double kprecision = -k_precision;                        //Fractional bits hold powers in negative exponents.
                         while(Binary != 0)                                      // Hence, they are initiated to the maximum precision value with negative exponents.
                             {
                                 if(Binary % 10 == 1)                
